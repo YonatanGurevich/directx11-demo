bb6ebbbda083f7be9b8d2691b0d67a2b946052a2
Yonatan Gurevich <jonatangurevich4@gmail.com>
2023-09-18 07:11:31 +0300

    Init D3D, Render Clear Color.

---
+++ main.cpp
@@ -0,0 +1,164 @@
+// include the basic windows header file
+#include <windows.h>
+#include <windowsx.h>
+#include <d3d11.h>
+
+#pragma comment (lib, "d3d11.lib")
+
+IDXGISwapChain* swapChain;
+ID3D11Device* device;
+ID3D11DeviceContext* deviceContext;
+
+void InitD3D(HWND hWnd);
+void CleanD3D(void);
+
+ID3D11RenderTargetView* backBuffer;
+
+void InitD3D(HWND hWnd) {
+    DXGI_SWAP_CHAIN_DESC scd;
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+    scd.BufferCount = 1;
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    scd.OutputWindow = hWnd;
+    scd.SampleDesc.Count = 4;
+    scd.Windowed = true;
+    D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, NULL, NULL, D3D11_SDK_VERSION, &scd, &swapChain, &device, NULL, &deviceContext);
+
+    ID3D11Texture2D* pBackBuffer;
+    swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    // Set the render target
+    device->CreateRenderTargetView(pBackBuffer, NULL, &backBuffer);
+    pBackBuffer->Release();
+    deviceContext->OMSetRenderTargets(1, &backBuffer, NULL);
+
+    // Set the viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = 800;
+    viewport.Height = 600;
+    deviceContext->RSSetViewports(1, &viewport);
+}
+
+void CleanD3D(void) {
+    swapChain->Release();
+    backBuffer->Release();
+    device->Release();
+    deviceContext->Release();
+}
+
+void RenderFrame(void) {
+    float clearColor[4] = { 1.0f, 0.4f, 0.3f, 1.0f };
+    deviceContext->ClearRenderTargetView(backBuffer, clearColor);
+    swapChain->Present(0, 0);
+}
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam);
+
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    // the handle for the window, filled by a function
+    HWND hWnd;
+    // this struct holds information for the window class
+    WNDCLASSEX wc;
+
+    // clear out the window class for use
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    // fill in the struct with the needed information
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = L"WindowClass1";
+
+    // register the window class
+    RegisterClassEx(&wc);
+
+    RECT wr = { 0, 0, 500, 400 };    // set the size, but not the position
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);    // adjust the size
+
+    // create the window and use the result as the handle
+    hWnd = CreateWindowEx(NULL,
+        L"WindowClass1",    // name of the window class
+        L"Our First Windowed Program",   // title of the window
+        WS_OVERLAPPEDWINDOW,    // window style
+        300,    // x-position of the window
+        300,    // y-position of the window
+        wr.right - wr.left,    // width of the window
+        wr.bottom - wr.top,    // height of the window
+        NULL,    // we have no parent window, NULL
+        NULL,    // we aren't using menus, NULL
+        hInstance,    // application handle
+        NULL);    // used with multiple windows, NULL
+
+    // display the window on the screen
+    ShowWindow(hWnd, nCmdShow);
+
+    InitD3D(hWnd);
+    // enter the main loop:
+
+    // this struct holds Windows event messages
+    MSG msg = { 0 };
+
+    // Enter the infinite message loop
+    while (TRUE)
+    {
+        // Check to see if any messages are waiting in the queue
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            // translate keystroke messages into the right format
+            TranslateMessage(&msg);
+
+            // send the message to the WindowProc function
+            DispatchMessage(&msg);
+
+            // check to see if it's time to quit
+            if (msg.message == WM_QUIT)
+                break;
+        }
+        RenderFrame();
+        //else
+        //{
+        //    // Run game code here
+        //    // ...
+        //    // ...
+        //}
+    }
+
+    CleanD3D();
+    // return this part of the WM_QUIT message to Windows
+    return msg.wParam;
+}
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    // sort through and find what code to run for the message given
+    switch (message)
+    {
+        // this message is read when the window is closed
+    case WM_DESTROY:
+    {
+        // close the application entirely
+        PostQuitMessage(0);
+        return 0;
+    } break;
+    }
+
+    // Handle any messages the switch statement didn't
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
